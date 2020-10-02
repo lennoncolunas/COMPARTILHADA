20e333db3a5a87c2d8c841050f1a492a1401b49b
lennon.colunas <lennon.colunas@gmail.com>
2020-10-02 16:11:38 -0300

    02102020_1611__~0,2%date:~3,2%date:~6,4%hora:~0,2%time:~3,2%time:~6,2

--- comitando.bat
+++ comitando.bat
@@ -1,5 +1,5 @@
 @echo off
 git add .
-echo %data:~0,2%%date:~3,2%%date:~6,4%%hora:~0,2%%time:~3,2%%time:~6,2%
+echo %date:~0,2%%date:~3,2%%date:~6,4%%hora:~0,2%%time:~3,2%%time:~6,2%
 git commit -m %date:~0,2%%date:~3,2%%date:~6,4%_%time:~0,2%%time:~3,2%__%data:~0,2%%date:~3,2%%date:~6,4%%hora:~0,2%%time:~3,2%%time:~6,2%
 git push 
